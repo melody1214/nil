7aa56df9b226d61f58c6f8479977c09ae6bf6f34
chanyoung <park910113@gmail.com>
2017-10-31 13:20:48 +0900

    using cobra for cli

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,6 +1,10 @@
 package cli
 
+import (
+	"github.com/chanyoung/nil/pkg/cmd"
+)
+
 // Main is the entry point for the cli.
 func Main() {
-
+	cmd.RootCmd.Execute()
 }
---
+++ pkg/cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// RootCmd is a root of all commands.
+var RootCmd = &cobra.Command{
+	Use:   "nil [command] [flags]",
+	Short: "nil command-line interface",
+	Long:  `nil command-line interface`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("cli work!!")
+	},
+}
+
+func init() {
+	// Add commands.
+}
