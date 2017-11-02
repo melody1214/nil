6acfd27fbb58883b294f2ec28d551e1bd68f6d03
chanyoung <park910113@gmail.com>
2017-11-02 17:33:58 +0900

    db: use short variable name

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -6,8 +6,8 @@ import (
 
 // BasicDB is for testing in-memory key-value store.
 type BasicDB struct {
-	data  map[string]interface{}
-	mutex sync.RWMutex
+	data map[string]interface{}
+	mu   sync.RWMutex
 }
 
 // New returns a new BasicDB object.
@@ -19,24 +19,24 @@ func New() *BasicDB {
 
 // Put inserts value into the BasicDB with the given key.
 func (b *BasicDB) Put(key string, val interface{}) {
-	b.mutex.Lock()
-	defer b.mutex.Unlock()
+	b.mu.Lock()
+	defer b.mu.Unlock()
 
 	b.data[key] = val
 }
 
 // Get returns value of given key, nil otherwise.
 func (b *BasicDB) Get(key string) interface{} {
-	b.mutex.RLock()
-	defer b.mutex.RUnlock()
+	b.mu.RLock()
+	defer b.mu.RUnlock()
 
 	return b.data[key]
 }
 
 // Delete deletes the element with the given key.
 func (b *BasicDB) Delete(key string) {
-	b.mutex.Lock()
-	defer b.mutex.Unlock()
+	b.mu.Lock()
+	defer b.mu.Unlock()
 
 	delete(b.data, key)
 }
