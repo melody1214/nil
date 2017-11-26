6c371246ee8f1d7feb9b2961624c5df4f3531bfe
chanyoung <park910113@gmail.com>
2017-11-26 17:55:22 +0900

    mds: add mysql flags

--- pkg/cmd/mds.go
+++ pkg/cmd/mds.go
@@ -31,5 +31,10 @@ func mdsRun(cmd *cobra.Command, args []string) {
 func init() {
 	mdsCmd.Flags().StringVarP(&mdscfg.ServerAddr, "bind", "b", config.Get("mds.addr"), "address to which the mds will bind")
 	mdsCmd.Flags().StringVarP(&mdscfg.ServerPort, "port", "p", config.Get("mds.port"), "port on which the mds will listen")
+	mdsCmd.Flags().StringVarP(&mdscfg.MySQLUser, "mysql-user", "", config.Get("mds.mysql_user"), "user id to mysql server")
+	mdsCmd.Flags().StringVarP(&mdscfg.MySQLPassword, "mysql-password", "", config.Get("mds.mysql_password"), "password of mysql user")
+	mdsCmd.Flags().StringVarP(&mdscfg.MySQLHost, "mysql-host", "", config.Get("mds.mysql_host"), "host address of mysql server")
+	mdsCmd.Flags().StringVarP(&mdscfg.MySQLPort, "mysql-port", "", config.Get("mds.mysql_port"), "port number of mysql server")
+	mdsCmd.Flags().StringVarP(&mdscfg.MySQLDatabase, "mysql-database", "", config.Get("mds.mysql_database"), "mysql schema name")
 	mdsCmd.Flags().StringVarP(&mdscfg.LogLocation, "log", "l", config.Get("mds.log_location"), "log location of the mds will print out")
 }
--- pkg/util/config/mdscfg.go
+++ pkg/util/config/mdscfg.go
@@ -7,9 +7,19 @@ type Mds struct {
 
 	// ServerAddr is the address of the metadata server.
 	ServerAddr string
-
 	// ServerPort is the port of the metadata server.
 	ServerPort string
+
+	// MySQLUser is the user ID of MySQL database.
+	MySQLUser string
+	// MySQLPassword is the password of MySQL user.
+	MySQLPassword string
+	// MySQLDatabase is the schema name.
+	MySQLDatabase string
+	// MySQLHost is the host address of MySQL server.
+	MySQLHost string
+	// MySQLPort is the port number of MySQL server.
+	MySQLPort string
 
 	// LogLocation is the file path of mds logging.
 	// Default output path is stderr.
