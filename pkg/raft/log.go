a945b45c30b6fa90cc8380325221da7cc35f67f7
chanyoung <park910113@gmail.com>
2017-12-07 18:24:37 +0900

    raft: add basic logStore interface

--- pkg/raft/log.go
+++ pkg/raft/log.go
@@ -1,7 +1,19 @@
 package raft
 
-// logs store.
-type log struct {
+// logEntry is the entry of the raft logs.
+type logEntry struct {
 	index uint64
-	data  []string
+	query string
+}
+
+// logStore is the store of the raft logs.
+type logStore interface {
+	// lastIndex returns the last applied index.
+	lastIndex() (index uint64)
+
+	// readLog returns a log entry with the given index.
+	readLog(index uint64) (logEntry *logEntry, err error)
+
+	// writeLog write a log entry into the store.
+	writeLog(logEntry *logEntry) error
 }
