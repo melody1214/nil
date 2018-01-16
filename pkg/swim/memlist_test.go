6ff431785767ce75511fadc2d99930df8cc49af1
chanyoung <park910113@gmail.com>
2018-01-15 21:52:15 -0800

    fix changed field name

--- pkg/swim/memlist_test.go
+++ pkg/swim/memlist_test.go
@@ -8,23 +8,23 @@ func TestMemlist(t *testing.T) {
 	meml := newMemList()
 
 	testCases := []*Member{
-		&Member{UUID: "1"},
-		&Member{UUID: "2"},
-		&Member{UUID: "3"},
+		&Member{ID: "1"},
+		&Member{ID: "2"},
+		&Member{ID: "3"},
 	}
 
 	for i, c := range testCases {
-		if v := meml.get(c.UUID); v != nil {
-			t.Errorf("expected value of %s to be nil: got %+v", c.UUID, v)
+		if v := meml.get(c.ID); v != nil {
+			t.Errorf("expected value of %s to be nil: got %+v", c.ID, v)
 		}
 
 		meml.set(c)
-		if v := meml.get(c.UUID); v == c {
-			t.Errorf("expected copied object of %s: got same object", c.UUID)
+		if v := meml.get(c.ID); v == c {
+			t.Errorf("expected copied object of %s: got same object", c.ID)
 		}
 
-		if v := meml.get(c.UUID); *v != *c {
-			t.Errorf("expected value of %s to be %+v: got %+v", c.UUID, c, v)
+		if v := meml.get(c.ID); *v != *c {
+			t.Errorf("expected value of %s to be %+v: got %+v", c.ID, c, v)
 		}
 
 		if v := meml.fetch(i + 1); len(v) != i+1 {
