5c742d9a9c95cb64744ef56542f1ab42f10fe328
chanyoung <park910113@gmail.com>
2018-03-06 01:46:12 +0900

    add ds table

--- pkg/mds/mysql/generate-base.go
+++ pkg/mds/mysql/generate-base.go
@@ -6,8 +6,8 @@ var generateSQLBase = []string{
 		CREATE TABLE IF NOT EXISTS user (
 			user_id bigint unsigned NOT NULL AUTO_INCREMENT,
 			user_name varchar(128) CHARACTER SET ascii NOT NULL,
-			access_key varchar(32) charset ascii not null,
-			secret_key varchar(32) charset ascii not null,
+			access_key varchar(32) charset ascii NOT NULL,
+			secret_key varchar(32) charset ascii NOT NULL,
 			PRIMARY KEY (user_id)
 		) ENGINE=InnoDB DEFAULT CHARSET=ascii
 	`,
@@ -29,6 +29,28 @@ var generateSQLBase = []string{
 			UNIQUE KEY (bucket_name, region_id),
 			FOREIGN KEY (user_id) REFERENCES user (user_id),
 			FOREIGN KEY (region_id) REFERENCES region (region_id)
+		) ENGINE=InnoDB DEFAULT CHARSET=ascii
+	`,
+	`
+		CREATE TABLE IF NOT EXISTS ds (
+			ds_id bigint unsigned NOT NULL AUTO_INCREMENT,
+			ds_name varchar(32) CHARACTER SET ascii NOT NULL,
+			PRIMARY KEY (ds_id),
+			UNIQUE KEY (ds_name)
+		) ENGINE=InnoDB DEFAULT CHARSET=ascii
+	`,
+	`
+		CREATE TABLE IF NOT EXISTS volume (
+			volume_id bigint unsigned NOT NULL AUTO_INCREMENT,
+			volume_name varchar(32) CHARACTER SET ascii NOT NULL,
+			ds_id bigint unsigned NOT NULL,
+			size int unsigned NOT NULL,
+			free int unsigned NOT NULL,
+			used int unsigned NOT NULL,
+			speed varchar(32) charset ascii NOT NULL,
+			PRIMARY KEY (volume_id),
+			UNIQUE KEY (volume_name),
+			FOREIGN KEY (ds_id) REFERENCES ds (ds_id)
 		) ENGINE=InnoDB DEFAULT CHARSET=ascii
 	`,
 }
