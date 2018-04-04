a7d9e8f71c9f2279f57220bdfcc7e2459cc067fa
chanyoung <park910113@gmail.com>
2018-04-04 17:50:10 +0900

    avoid init config file while on test

--- pkg/util/config/config.go
+++ pkg/util/config/config.go
@@ -13,10 +13,13 @@ var config *gabs.Container
 // Get returns config data with the given path.
 // Config data is only allowed in string type.
 func Get(path string) string {
+	if config == nil {
+		setConfig()
+	}
 	return config.Path(path).Data().(string)
 }
 
-func init() {
+func setConfig() {
 	json, err := gabs.ParseJSONFile("/etc/nil/" + configFile)
 	if err != nil {
 		log.Panic(err)
