3f0315d13db9a483766eef47e64b9bba3567b6c4
chanyoung <park910113@gmail.com>
2018-06-26 21:31:19 +0900

    change virtual to experiment

--- virtual.sh
+++ virtual.sh
@@ -13,18 +13,20 @@ MNT=$DIR/mnt
 PENDINGCMD=$DIR/pending
 
 # Region names follow ISO-3166-1
-# REGIONS=("idckr" "idcus" "idchk" "idcsg" "idcjp" "idcde")
-REGIONS=("idckr")
+REGIONS=("idckr" "idcus" "idchk" "idcsg" "idcjp" "idcde")
+# REGIONS=("idckr")
 GW=1
 MDS=1
-DS=15
+DS=10
 GWBASEPORT=50000
 MDSBASEPORT=51000
 DSBASEPORT=52000
 
-# Disk configuration.
-DISKSIZE=400 # megabytes
-DISKNUM=5    # per ds
+# # Disk configuration.
+# DISKSIZE=400 # megabytes
+# DISKNUM=5    # per ds
+DISKSIZE=200
+DISKNUM=2
 
 # User per region
 TOTALUSERS=0    # (REGIONUSERS) * (number of regions)
@@ -494,7 +496,7 @@ function main() {
     creatediskall
 
     # Generate global encoding group.
-    # ggg 300
+    ggg 300
 
     # Execute pending command.
     if [ -e $PENDINGCMD ]; then
@@ -507,29 +509,29 @@ function main() {
         done < $PENDINGCMD
     fi
 
-    # # Create users.
-    # sleep 3
-    # createusers
+    # Create users.
+    sleep 3
+    createusers
 
-    # # Create buckets.
-    # sleep 5
-    # createbuckets
+    # Create buckets.
+    sleep 5
+    createbuckets
 
-    # # Put objects.
-    # sleep 15
-    # putobjects
+    # Put objects.
+    sleep 15
+    putobjects
 
-    # # Get objects.
-    # echo -n "Do you want to download all chunks? [y/n] "
-    # read ANSWER
-    # case $ANSWER in
-    #     y|Y)
-    #         echo " Start downloads."
-    #         getobjects
-    #         ;;
-    #     *)
-    #         echo " No downloads."
-    # esac
+    # Get objects.
+    echo -n "Do you want to download all chunks? [y/n] "
+    read ANSWER
+    case $ANSWER in
+        y|Y)
+            echo " Start downloads."
+            getobjects
+            ;;
+        *)
+            echo " No downloads."
+    esac
 
     # # Test local recovery
     # if [ $TESTLOCALRECOVERY = true ]; then
