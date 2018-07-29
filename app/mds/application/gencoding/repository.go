dc29d986f9306cac7e7d5f72603d589384353145
chanyoung <park910113@gmail.com>
2018-07-30 07:20:14 +0900

    remove gencoding

--- app/mds/application/gencoding/repository.go
+++ app/mds/application/gencoding/repository.go
@@ -1,31 +1,13 @@
 package gencoding
 
-import (
-	"github.com/chanyoung/nil/app/mds/application/gencoding/token"
-	"github.com/chanyoung/nil/app/mds/infrastructure/repository"
-	"github.com/chanyoung/nil/pkg/cmap"
-)
+import "github.com/chanyoung/nil/app/mds/infrastructure/repository"
 
 // Repository provides access to gencoding database.
 type Repository interface {
 	Begin() (repository.TxID, error)
 	Rollback(repository.TxID) error
 	Commit(repository.TxID) error
-	GenerateGencodingGroup(regions []string) error
-	// UpdateUnencodedChunks(regionName string, unencoded int) error
 	AmILeader() (bool, error)
 	LeaderEndpoint() string
-	// Make() (*Table, error)
 	RegionEndpoint(regionID int) (endpoint string)
-	GetRoutes(leaderEndpoint string) (*token.Leg, error)
-	MakeGlobalEncodingJob(token *token.Token, primary *token.Unencoded) error
-	GetJob(regionName string) (*token.Token, error)
-	SetJobStatus(id int64, status Status) error
-	RemoveFailedJobs() error
-	JobFinished(*token.Token) error
-	UpdateUnencoded(egs []cmap.EncodingGroup) ([]cmap.EncodingGroup, error)
-	GetChunk(eg cmap.ID) (cID string, err error)
-	SetChunk(cID string, egID cmap.ID, status string) error
-	GetCandidateChunk(egID cmap.ID, region string) (cID string, err error)
-	SetPrimaryChunk(token.Unencoded, int64) error
 }
